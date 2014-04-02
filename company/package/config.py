dac4faa74737d402648a708785b4185a1e96a01a
Pedro Emanuel de Castro Faria Salgado <pedro.salgado@mediaforge.com>
2014-04-02 17:08:18 -0600

    added generic config module and its test case.

---
+++ company/package/config.py
@@ -0,0 +1,70 @@
+"""
+.. module:: package.config
+    :platform: Unix
+    :synopsis: Configuration utilities.
+
+.. moduleauthor:: Your Name <email address>
+"""
+
+from __future__ import absolute_import
+
+import os.path
+
+try:
+    from ConfigParser import SafeConfigParser as ConfigParser, NoSectionError
+except ImportError:
+    from configparser import ConfigParser, NoSectionError
+
+import logging
+
+
+DEFAULT_CONFIG_FILE = '/etc/package/package.cfg'
+
+settings = None
+
+
+def load_configuration(config_file=DEFAULT_CONFIG_FILE):
+    """
+    Loads configuration.
+
+    :param config_file: the configuration file(s).
+    :type config_file: str or list of str
+    """
+    global settings
+
+    if not os.path.exists(config_file) or not os.path.isfile(config_file):
+        msg = '%s configuration file does not exist!' % config_file
+        logging.getLogger(__name__).error(msg)
+        raise ValueError(msg)
+
+    settings = ConfigParser()
+    try:
+        settings.read(config_file)
+        logging.getLogger().info('%s configuration file was loaded.' % config_file)
+    except NoSectionError as e:
+        msg = 'Failed to load configuration from %s!' % config_file
+        logging.getLogger(__name__).error(msg)
+        logging.getLogger(__name__).debug(str(e), exc_info=True)
+
+
+def get():
+    """
+    Returns the configuration.
+
+    :return: the configuration.
+    :rtype: object (configParser.ConfigParser)
+    """
+    global settings
+
+    if settings is None:
+        load_configuration()
+    return settings
+
+
+def reset():
+    """
+    Reset the configuration.
+    """
+    global settings
+
+    settings = None
---
+++ tests/config_test.py
@@ -0,0 +1,92 @@
+import company.package.config
+import ConfigParser
+
+import mock
+
+import pytest
+
+import unittest
+
+
+from company.package.config import DEFAULT_CONFIG_FILE
+
+
+class ConfigTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # mock of logging.RootLogger
+        self.patch_get_logger = mock.patch('company.package.config.logging.getLogger')
+
+        self.mock_get_logger = self.patch_get_logger.start()
+
+        self.instance = self.mock_get_logger.return_value
+        self.instance.debug.return_value = None
+        self.instance.error.return_value = None
+        self.instance.info.return_value = None
+
+        self.addCleanup(self.patch_get_logger.stop)
+
+#    def tearDown(self):
+#        mock.patch.stopall()
+
+    @mock.patch('company.package.config.os.path')
+    @mock.patch('company.package.config.ConfigParser.read')
+    def test_load_configuration(self, mock_read, mock_path):
+        """
+        Test company.logging.load().
+        """
+        mock_path.exists.return_value = True
+        mock_path.isfile.return_value = True
+        mock_read.return_value = None
+
+        company.package.config.load_configuration()
+
+        mock_path.exists.assert_called_once_with(DEFAULT_CONFIG_FILE)
+        mock_path.isfile.assert_called_once_with(DEFAULT_CONFIG_FILE)
+        mock_read.assert_called_with(DEFAULT_CONFIG_FILE)
+
+        self.assertTrue(self.mock_get_logger.called)
+        self.instance.info.assert_called_once_with('%s configuration file was loaded.' % DEFAULT_CONFIG_FILE)
+
+    @mock.patch('company.package.config.os.path')
+    @mock.patch('company.package.config.ConfigParser.read')
+    def test_load_configuration_nofile(self, mock_read, mock_path):
+        """
+        Test company.logging.load() when the configuration file doesn't exist.
+        """
+        mock_path.exists.return_value = True
+        mock_path.isfile.return_value = False
+        mock_read.return_value = None
+
+        with pytest.raises(ValueError):
+            company.package.config.load_configuration()
+
+        mock_path.exists.assert_called_once_with(DEFAULT_CONFIG_FILE)
+        mock_path.isfile.assert_called_once_with(DEFAULT_CONFIG_FILE)
+
+        self.assertFalse(mock_read.readConfig.called)
+
+        self.assertTrue(self.mock_get_logger.called)
+        self.instance.error.assert_called_once_with('%s configuration file does not exist!' % DEFAULT_CONFIG_FILE)
+
+    @mock.patch('company.package.config.os.path')
+    @mock.patch('company.package.config.ConfigParser.read')
+    def test_load_configuration_errors(self, mock_read, mock_path):
+        """
+        Test company.logging.load() when NoSectionErrors are raised.
+        """
+        mock_path.exists.return_value = True
+        mock_path.isfile.return_value = True
+        mock_read.side_effect = ConfigParser.NoSectionError("No section: 'formatters'")
+
+        company.package.config.load_configuration()
+
+        mock_path.exists.assert_called_once_with(DEFAULT_CONFIG_FILE)
+        mock_path.isfile.assert_called_once_with(DEFAULT_CONFIG_FILE)
+
+        self.assertFalse(mock_read.readConfig.called)
+
+        self.assertTrue(self.mock_get_logger.called)
+        self.instance.error.assert_called_once_with('Failed to load configuration from %s!' % DEFAULT_CONFIG_FILE)
+        self.instance.debug.assert_called_once_with(
+            str(ConfigParser.NoSectionError("No section: 'formatters'")), exc_info=True)
