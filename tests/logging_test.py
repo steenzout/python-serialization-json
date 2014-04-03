0d5e45d639cd37703381c5f2093758afda2acbd3
Pedro Emanuel de Castro Faria Salgado <pedro.salgado@mediaforge.com>
2014-04-03 10:51:06 -0600

    renamed self.instance to self.mock_root_logger. changed imports to better match new logic on company.package.logging package.

--- tests/logging_test.py
+++ tests/logging_test.py
@@ -1,54 +1,89 @@
-import sys
-import os.path
-
 import company.package.logging
+
 import ConfigParser
 
-import logging
-
 import mock
+
+import pytest
 
 import unittest
 
 
-from company.package.logging import DEFAULT_LOGGING_CONFIG_FILE
+from company.package.logging import DEFAULT_CONFIG_FILE
 
 
-class LoggingTestCase(unittest.TestCase):
+class ConfigTestCase(unittest.TestCase):
 
-    def test_load_configuration(self):
+    def setUp(self):
+        # mock of logging.RootLogger
+        self.patch_get_logger = mock.patch('company.package.logging.logging.getLogger')
+        self.mock_get_logger = self.patch_get_logger.start()
+
+        self.mock_root_logger = self.mock_get_logger.return_value
+        self.mock_root_logger.debug.return_value = None
+        self.mock_root_logger.error.return_value = None
+        self.mock_root_logger.info.return_value = None
+
+        self.addCleanup(self.patch_get_logger.stop)
+
+    @mock.patch('company.package.logging.os.path')
+    @mock.patch('company.package.logging.logging.config.fileConfig')
+    def test_load_configuration(self, mock_fileConfig, mock_path):
         """
-        Test company.logging.load().
+        Test company.package.logging.load_configuration().
         """
-        with mock.patch.object(os.path, 'exists', return_value=True) as mock_method1:
-            with mock.patch.object(logging.config, 'fileConfig', return_value=None) as mock_method2:
-                company.package.logging.load_configuration()
+        mock_path.exists.return_value = True
+        mock_path.isfile.return_value = True
+        mock_fileConfig.return_value = None
 
-        mock_method1.assert_called_with(DEFAULT_LOGGING_CONFIG_FILE)
-        mock_method2.assert_called_with(DEFAULT_LOGGING_CONFIG_FILE, disable_existing_loggers=False)
+        company.package.logging.load_configuration()
 
-    def test_load_configuration_nofile(self):
+        mock_path.exists.assert_called_once_with(DEFAULT_CONFIG_FILE)
+        mock_path.isfile.assert_called_once_with(DEFAULT_CONFIG_FILE)
+        mock_fileConfig.assert_called_with(DEFAULT_CONFIG_FILE, disable_existing_loggers=False)
+
+        self.assertTrue(self.mock_get_logger.called)
+        self.mock_root_logger.info.assert_called_once_with('%s configuration file was loaded.' % DEFAULT_CONFIG_FILE)
+
+    @mock.patch('company.package.logging.os.path')
+    @mock.patch('company.package.logging.logging.config.fileConfig')
+    def test_load_configuration_nofile(self, mock_fileConfig, mock_path):
         """
-        Test company.logging.load() when the configuration file doesn't exist.
+        Test company.package.logging.load_configuration() when the configuration file doesn't exist.
         """
-        with mock.patch.object(os.path, 'exists', return_value=False) as mock_method1:
-            with mock.patch.object(sys.stderr, 'write', return_value=None) as mock_method2:
-                company.package.logging.load_configuration()
+        mock_path.exists.return_value = True
+        mock_path.isfile.return_value = False
+        mock_fileConfig.return_value = None
 
-        mock_method1.assert_called_with(DEFAULT_LOGGING_CONFIG_FILE)
-        mock_method2.assert_called_with(
-            'Logging configuration file %s doesn\'t exist!\n' % DEFAULT_LOGGING_CONFIG_FILE)
+        with pytest.raises(ValueError):
+            company.package.logging.load_configuration()
 
-    def test_load_configuration_errors(self):
+        mock_path.exists.assert_called_once_with(DEFAULT_CONFIG_FILE)
+        mock_path.isfile.assert_called_once_with(DEFAULT_CONFIG_FILE)
+
+        self.assertFalse(mock_fileConfig.fileConfig.called)
+
+        self.assertTrue(self.mock_get_logger.called)
+        self.mock_root_logger.error.assert_called_once_with('%s configuration file does not exist!' % DEFAULT_CONFIG_FILE)
+
+    @mock.patch('company.package.logging.os.path')
+    @mock.patch('company.package.logging.logging.config.fileConfig')
+    def test_load_configuration_errors(self, mock_fileConfig, mock_path):
         """
-        Test company.logging.load() when NoSectionErrors are raised.
+        Test company.package.logging.load_configuration() when NoSectionErrors are raised.
         """
-        with mock.patch.object(os.path, 'exists', return_value=True) as mock_method1:
-            with mock.patch.object(logging.config, 'fileConfig', return_value=None) as mock_method2:
-                with mock.patch.object(sys.stderr, 'write', return_value=None) as mock_method3:
-                    mock_method2.side_effect = ConfigParser.NoSectionError("No section: 'formatters'")
-                    company.package.logging.load_configuration()
+        mock_path.exists.return_value = True
+        mock_path.isfile.return_value = True
+        mock_fileConfig.side_effect = ConfigParser.NoSectionError("No section: 'formatters'")
 
-        mock_method1.assert_called_with(DEFAULT_LOGGING_CONFIG_FILE)
-        mock_method2.assert_called_with(DEFAULT_LOGGING_CONFIG_FILE, disable_existing_loggers=False)
-        mock_method3.assert_called()
+        company.package.logging.load_configuration()
+
+        mock_path.exists.assert_called_once_with(DEFAULT_CONFIG_FILE)
+        mock_path.isfile.assert_called_once_with(DEFAULT_CONFIG_FILE)
+
+        self.assertFalse(mock_fileConfig.fileConfig.called)
+
+        self.assertTrue(self.mock_get_logger.called)
+        self.mock_root_logger.error.assert_called_once_with('Failed to load configuration from %s!' % DEFAULT_CONFIG_FILE)
+        self.mock_root_logger.debug.assert_called_once_with(
+            str(ConfigParser.NoSectionError("No section: 'formatters'")), exc_info=True)
