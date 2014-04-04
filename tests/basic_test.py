987d24fef3a8cf00ef65c0dcc2a37eaf06eef231
Pedro Emanuel de Castro Faria Salgado <pedro.salgado@mediaforge.com>
2014-04-04 16:33:24 -0600

    assert if configuration has been loaded.

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -5,7 +5,7 @@ class ATestCase(tests.BaseTestCase):
 
     def test(self):
         self.assertTrue(self.logging_loaded)
-        self.assertTrue(self.logging_loaded)
+        self.assertTrue(self.configuration_loaded)
 
         self.assertFalse(self.logger is not None)
 
