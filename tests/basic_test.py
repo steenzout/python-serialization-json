b6c3083d780ae24764b2dbf1291482e1f54aad0c
Pedro Emanuel de Castro Faria Salgado <pedro.salgado@mediaforge.com>
2014-04-04 16:53:33 -0600

    added assertion that configuration key was loaded.

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -10,4 +10,5 @@ class ATestCase(tests.BaseTestCase):
         self.assertTrue(self.configuration is not None)
 
     def test_configuration_contents(self):
+        self.assertTrue('key' in self.configuration['company.package'])
         self.assertEquals(self.configuration['company.package']['key'], 'value')
