04fa3643d629f7243bf80ee9db7adda6ffe717d3
Pedro Salgado <steenzout@ymail.com>
2016-08-16 11:21:19 -0600

    updated code on namespace packages.

--- steenzout/__init__.py
+++ steenzout/__init__.py
@@ -18,4 +18,9 @@
 """
 Steenzout namespace json.
 """
-__import__('pkg_resources').declare_namespace(__name__)
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
--- steenzout/serialization/__init__.py
+++ steenzout/serialization/__init__.py
@@ -17,4 +17,9 @@
 """
 Serialization namespace package.
 """
-__import__('pkg_resources').declare_namespace(__name__)
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
